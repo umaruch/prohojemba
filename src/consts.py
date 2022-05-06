37bf611c5b6ac83455ce41ee1583c3e00ba169a8
umaruch <60070943+umaruch@users.noreply.github.com>
2022-05-06 23:07:42 +0500

    add  model consts

---
+++ src/consts.py
@@ -0,0 +1,10 @@
+# Перечень возможных статусов активности
+ACTIVITY_STATUS = [
+    "planned", "active", "complete"
+]
+
+
+# Перечень типов тайтлов
+TITLE_TYPES = [
+    "game", "movie", "series", "anime", "book", "comic", "manga"
+]
