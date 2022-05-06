40374ee8d27928b28aa5b8489ad3e672d835ce2b
umaruch <60070943+umaruch@users.noreply.github.com>
2022-05-06 22:50:16 +0500

    Update security.py

--- src/services/security.py
+++ src/services/security.py
@@ -7,7 +7,6 @@ from src.core.settings import settings
 
 
 pwd_context = CryptContext(schemes=["bcrypt"])
-
 oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.application.API_URL}/auth/token"
 )
