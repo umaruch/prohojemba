353fe524103e8192ee5cbb17a55615a19a55f4de
umaruch <60070943+umaruch@users.noreply.github.com>
2022-05-08 01:26:12 +0500

    add endpoints functions with tags

--- src/api/endpoints/auth.py
+++ src/api/endpoints/auth.py
@@ -4,36 +4,36 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.post("/signin")
+@router.post("/signin", tags=["Авторизация"])
 async def signin():
     return "Hello, World"
 
 
-@router.post("/token")
+@router.post("/token", tags=["Авторизация"])
 async def token():
     pass
 
 
-@router.post("/token/update")
+@router.post("/token/update", tags=["Авторизация"])
 async def update_tokens_pair():
     pass
 
 
-@router.post("/email/change")
+@router.post("/email/change", tags=["Авторизация"])
 async def chenge_user_email():
     pass
 
 
-@router.post("/password/change")
+@router.post("/password/change", tags=["Авторизация"])
 async def change_user_password():
     pass
 
 
-@router.post("/password/restore")
+@router.post("/password/restore", tags=["Авторизация"])
 async def restore_user_password():
     pass
 
 
-@router.post("/email/validate")
+@router.post("/email/validate", tags=["Авторизация"])
 async def validate_email():
     pass
--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -9,7 +9,7 @@ from src.repositories.users import UsersRepository
 router = APIRouter()
 
 
-@router.get("/@me")
+@router.get("/@me", tags=["Пользователи"])
 async def get_current_user(
     users_repo: UsersRepository = Depends(get_repository(UsersRepository)), 
     redis_cache: Redis = Depends(get_redis_connection)
@@ -17,3 +17,34 @@ async def get_current_user(
     await redis_cache.get("test")
     await users_repo.test()
     return {"status": "ok"}
+
+
+@router.patch("/@me", tags=["Пользователи"])
+async def change_current_user_profile():
+    pass
+
+
+@router.get("/@me/reviews", tags=["Пользователи"])
+async def get_current_user_reviews():
+    pass
+
+
+@router.get("/@me/activity", tags=["Пользователи"])
+async def get_current_user_activity():
+    pass
+
+
+@router.get("/{user_id}", tags=["Пользователи"])
+async def get_user_profile(user_id: int):
+    pass
+
+
+@router.get("/{user_id}/reviews", tags=["Пользователи"])
+async def get_user_reviews(user_id: int):
+    pass
+
+
+@router.get("/{user_id}/activity", tags=["Пользователи"])
+async def get_user_activity(user_id: int):
+    pass
+
